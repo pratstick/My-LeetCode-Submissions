78f9cea8cbe50ff9410a426a6d8f3840dbc691ab
Pratyush <pratyushk2005@outlook.com>
2026-07-09 23:21:25 +0530

    Time: 3 ms (37.53%) | Memory: 14.8 MB (77.78%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -3,20 +3,22 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        
+        vector<int> prev = matrix[0];
         for (int row = 1; row < n; row++) {
+            vector<int> curr(m,0);
             for (int col = 0; col < m; col++) {                
-                int bottom = matrix[row - 1][col];
+                int bottom = prev[col];
                 int bl = INT_MAX;
-                if(col-1>=0) bl = matrix[row - 1][col - 1];
+                if(col-1>=0) bl = prev[col - 1];
                 int br = INT_MAX;
-                if(col+1<m) br = matrix[row - 1][col + 1];
-                matrix[row][col] += min({bottom, bl, br});
+                if(col+1<m) br = prev[col + 1];
+                curr[col] = matrix[row][col] +  min({bottom, bl, br});
             }
+            prev = curr;
         }
         int mini = INT_MAX;
         for (int i = 0; i < m; i++) {
-            mini = min(mini, matrix[n - 1][i]);
+            mini = min(mini, prev[i]);
         }
         
         return mini;
