dc83e318398010403425fa3c1b1cf39ae4d169d3
Pratyush <pratyushk2005@outlook.com>
2026-07-09 15:43:04 +0530

    Time: 3 ms (65.72%) | Memory: 8.1 MB (16.31%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -4,10 +4,14 @@ public:
         vector<int> dp(n+1,0);
         if(n<0) return -1;
         if(n<=1) return n;
-        dp[1] =1;
+        int p1 = 1;
+        int p2 = 0;
+        int curr = 1;
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            curr = p1 + p2;
+            p2 = p1;
+            p1 = curr;
         }
-        return dp[n];
+        return curr;
     }
 };
