820556eb15da3c395f9d46eab7fa234af518127c
Pratyush <pratyushk2005@outlook.com>
2026-07-09 15:25:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (37.93%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,13 +1,9 @@
+int ans[] = {0,     1,     1,      2,      3,      5,      8,     13,
+             21,    34,    55,     89,     144,    233,    377,   610,
+             987,   1597,  2584,   4181,   6765,   10946,  17711, 28657,
+             46368, 75025, 121393, 196418, 317811, 514229, 832040};
+
 class Solution {
-private:
-    int f(int n, vector<int>& dp){
-        if(n<=1) return n;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = f(n-1,dp) + f(n-2,dp);
-    }
 public:
-    int fib(int n) {
-         vector<int>dp(n+1,-1);
-         return f(n,dp);
-    }
+    int fib(int n) {return ans[n];}
 };
