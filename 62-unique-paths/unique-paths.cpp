493a2a43005ef2d81ac8044c98f3f6c1881d1269
Pratyush <pratyushk2005@outlook.com>
2026-07-09 20:09:09 +0530

    Time: 3 ms (9.48%) | Memory: 9.4 MB (53.82%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,13 +1,19 @@
-class Solution {   
-public:
+class Solution {
+   private:
+    int noOfWays(int row, int col, int m, int n, vector<vector<int>>& dp) {
+        if (row == m - 1 && col == n - 1) return 1;
+        if (row >= m || col >= n) return 0;
+        if (dp[row][col] != -1) return dp[row][col];
+        int right = 0;
+        if (col + 1 < n) right = noOfWays(row, col + 1, m, n, dp);
+        int down = 0;
+        if (row + 1 < m) down = noOfWays(row + 1, col, m, n, dp);
+        return dp[row][col] = right + down;
+    }
+
+   public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int> (n,1));
-        dp[0][0] = 1;
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return noOfWays(0, 0, m, n, dp);
     }
 };
