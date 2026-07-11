fbdb9bab68e47f727d16ceaffc564e3774ecc686
Pratyush <pratyushk2005@outlook.com>
2026-07-11 22:30:36 +0530

    Time: 15 ms (50.44%) | Memory: 26 MB (22.46%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,11 +1,14 @@
 class Solution {
 private:
     const int MOD = 1e9 + 7;
+    int total = 0;
     int helper(int ind, int sum, vector<int>& nums,int n, int target,vector<vector<int>>&dp){
+        if (sum < 0 || sum > 2 * total) return 0;
         if(ind==n){
             if(sum==target) return 1;
             else return 0;
         }
+        
         if(dp[ind][sum]!=-1) return dp[ind][sum];
         int neg = helper(ind+1,sum+nums[ind],nums,n,target,dp); 
         int pos = helper(ind+1,sum-nums[ind],nums,n,target,dp);
@@ -14,7 +17,8 @@ private:
 public:
   int findTargetSumWays(vector<int>& nums,int target) {
         int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(6000,-1));
-        return helper(0,3000,nums,n,target+3000,dp);
+        total = accumulate(nums.begin(), nums.end(), 0); //offset
+        vector<vector<int>> dp(n, vector<int>(2 * total + 1, -1));
+        return helper(0,total,nums,n,target+total,dp);
  }
 };
