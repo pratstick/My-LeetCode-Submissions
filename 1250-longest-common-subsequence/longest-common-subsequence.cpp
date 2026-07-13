51ec1171a83ecbd609aaf4ccbb79f064b46d0058
Pratyush <pratyushk2005@outlook.com>
2026-07-13 22:29:58 +0530

    Time: 21 ms (83.98%) | Memory: 26.7 MB (80.35%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -7,13 +7,13 @@ public:
         for (int i = 1; i <= m; i++) {
             vector<int> curr(n + 1, 0);
             for (int j = 1; j <= n; j++) {
-                int match = 0;
                 if (text1[i - 1] == text2[j - 1]) {
-                    match = 1 + prev[j - 1];
+                    curr[j] = 1 + prev[j - 1];
+                    continue;
                 }
                 int skip_str1 = (i >= 1) ? prev[j] : 0;
                 int skip_str2 = (j >= 1) ? curr[j - 1] : 0;
-                curr[j] = max({match, skip_str1, skip_str2});
+                curr[j] = max(skip_str1, skip_str2);
             }
             prev = curr;
         }
