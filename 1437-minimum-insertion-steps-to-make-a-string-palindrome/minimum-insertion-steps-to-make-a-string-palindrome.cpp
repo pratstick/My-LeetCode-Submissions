190465a5a5f8179a2df1b01c1c7b4e1ab4a7b1d6
Pratyush <pratyushk2005@outlook.com>
2026-07-13 22:06:43 +0530

    Time: 61 ms (11.63%) | Memory: 31.7 MB (15.63%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,30 @@
+class Solution {
+   private:
+    int helper(int ind1, int ind2, const string& str1, const string& str2,
+               vector<vector<int>>& dp) {
+        if (ind1 < 0 || ind2 < 0) return 0;
+        if (dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        int match = 0;
+        if (str1[ind1] == str2[ind2]) {
+            return  dp[ind1][ind2] = match = 1 + helper(ind1 - 1, ind2 - 1, str1, str2, dp);
+        }
+        int skip_str1 = helper(ind1 - 1, ind2, str1, str2, dp);
+        int skip_str2 = helper(ind1, ind2 - 1, str1, str2, dp);
+        return dp[ind1][ind2] = max(skip_str1, skip_str2);
+    }
+
+    int lcs(string& str1, string& str2) {
+        int m = str1.size();
+        int n = str2.size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return helper(m - 1, n - 1, str1, str2, dp);
+    }
+    int longestPalinSubseq(string s) {
+        string reversed(s.rbegin(), s.rend());
+        return lcs(s,reversed);
+    }
+public:
+    int minInsertions(string s){
+        return s.size() - longestPalinSubseq(s);
+    }
+};
