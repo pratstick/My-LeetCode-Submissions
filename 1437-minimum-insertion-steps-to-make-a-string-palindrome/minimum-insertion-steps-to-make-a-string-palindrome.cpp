a6ba4ff3c82137c652a73b4165456128ef33b323
Pratyush <pratyushk2005@outlook.com>
2026-07-13 22:08:48 +0530

    Time: 22 ms (79.61%) | Memory: 9.8 MB (89.34%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -3,19 +3,21 @@ private:
     int lcs(const string& str1, const string& str2) {
         int m = str1.size();
         int n = str2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));     
+
+        vector<int> prev(n + 1, 0), curr(n + 1, 0);
 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (str1[i - 1] == str2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    curr[j] = 1 + prev[j - 1];
                 } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    curr[j] = max(prev[j], curr[j - 1]);
                 }
             }
+            prev = curr;
         }
-        
-        return dp[m][n];
+
+        return prev[n];
     }
 
     int longestPalinSubseq(string s) {
@@ -24,7 +26,5 @@ private:
     }
 
 public:
-    int minInsertions(string s) {
-        return s.size() - longestPalinSubseq(s);
-    }
+    int minInsertions(string s) { return s.size() - longestPalinSubseq(s); }
 };
