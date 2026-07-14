3b96300cc9a351f644844cd4454fe35c096693f0
Pratyush <pratyushk2005@outlook.com>
2026-07-14 21:23:23 +0530

    Time: 4 ms (81.36%) | Memory: 12.5 MB (82.60%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,22 +1,31 @@
 class Solution {
-    private:
-    int helper(int i, int j,string s1, string s2, vector<vector<int>>&dp){
-        if(j<0) return i+1;
-        if(i<0) return j+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int match = 1e9;
-        if(s1[i]==s2[j]) match = helper(i-1,j-1,s1,s2,dp);
-        int insert = 1 + helper(i,j-1,s1,s2,dp);
-        int del = 1 + helper(i-1,j,s1,s2,dp);
-        int replace = 1 + helper(i-1,j-1,s1,s2,dp);
-        return dp[i][j] = min({insert,del,replace,match});
-    }
   public:
-    int minDistance(string word1, string word2) {
-        int m = word1.size();
-        int n = word2.size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return helper(m-1,n-1,word1,word2,dp);
+    int minDistance(string start, string target) {
+        int m = start.size();
+        int n = target.size();
+        vector<int>prev(n+1,0);
+        for(int i=0;i<=m;i++){
+            vector<int>curr(n+1,0);
+            for(int j=0;j<=n;j++){
+                if(j==0){
+                    curr[j] = i;
+                    continue;
+                }
+                if(i==0){
+                    curr[j] = j;
+                    continue;
+                }
+                if(start[i-1]==target[j-1]) curr[j] = prev[j-1];
+                else{
+                    int insert = 1 + curr[j-1];
+                    int del = 1 + prev[j];
+                    int replace = 1 + prev[j-1];
+                    curr[j] = min({insert,del,replace});
+                }      
+            }
+            prev = curr;
+        }
+        return prev[n];
     }
 };
 
