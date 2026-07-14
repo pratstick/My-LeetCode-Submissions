558da31272191caf1d53a5395a4b69991461f759
Pratyush <pratyushk2005@outlook.com>
2026-07-14 11:32:37 +0530

    Time: 1644 ms (13.04%) | Memory: 409.9 MB (7.83%) - LeetSync

--- 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,23 +1,41 @@
 class Solution {
-private:
-    const int MOD = 1e9+7;
-    int helper(int ind, int g1, int g2, vector<int>& nums,vector<vector<vector<int>>>&dp){
-        if(ind<0){
-            return (g1!=0 && g1==g2);
-        }
-        if(dp[ind][g1][g2]!=-1) return dp[ind][g1][g2];
-        int notTake = helper(ind-1,g1,g2,nums,dp) % MOD;
-        int newg1 = (g1==0)?nums[ind]:gcd(g1,nums[ind]) ;
-        int take1 = helper(ind-1,newg1,g2,nums,dp)% MOD;
-        int newg2 = (g2==0)?nums[ind]:gcd(g2,nums[ind]);
-        int take2 = helper(ind-1,g1,newg2,nums,dp)% MOD;
-
-        return dp[ind][g1][g2] = (1LL * notTake + take1 + take2)% MOD;        
-    }
 public:
+    const int MOD = 1e9 + 7;
+
     int subsequencePairCount(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<vector<int>>>dp(n,(vector<vector<int>>(201,vector<int>(201,-1))));
-        return helper(n-1,0,0,nums,dp);
+
+        vector<vector<vector<int>>> dp(
+            n + 1, vector<vector<int>>(201, vector<int>(201, 0)));
+
+        dp[0][0][0] = 1;
+
+        for (int ind = 0; ind < n; ind++) {
+            for (int g1 = 0; g1 <= 200; g1++) {
+                for (int g2 = 0; g2 <= 200; g2++) {
+
+                    long long cur = dp[ind][g1][g2];
+                    if (cur == 0)
+                        continue;
+
+                    // Not take
+                    dp[ind + 1][g1][g2] = (dp[ind + 1][g1][g2] + cur) % MOD;
+
+                    // Take in seq1
+                    int ng1 = (g1 == 0) ? nums[ind] : gcd(g1, nums[ind]);
+                    dp[ind + 1][ng1][g2] = (dp[ind + 1][ng1][g2] + cur) % MOD;
+
+                    // Take in seq2
+                    int ng2 = (g2 == 0) ? nums[ind] : gcd(g2, nums[ind]);
+                    dp[ind + 1][g1][ng2] = (dp[ind + 1][g1][ng2] + cur) % MOD;
+                }
+            }
+        }
+
+        long long ans = 0;
+        for (int g = 1; g <= 200; g++)
+            ans = (ans + dp[n][g][g]) % MOD;
+
+        return ans;
     }
 };
