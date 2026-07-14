06be5b5090dbcd28e8d937c7b75124c0f5d5eb53
Pratyush <pratyushk2005@outlook.com>
2026-07-14 22:24:03 +0530

    Time: 54 ms (49.31%) | Memory: 12.9 MB (79.52%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -4,9 +4,9 @@ class Solution {
     bool isMatch(string str, string pat) {
         int n = str.size();
         int m = pat.size();
-        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
-        dp[0][0] = true;
-        for (int j = 1; j <= m; j++) {
+        vector<bool> prev(m + 1, false);
+        prev[0] = true;
+        for(int j = 1; j <= m; j++) {
             bool flag = true;
             for (int k = 1; k <= j; k++) {
                 if (pat[k - 1] != '*') {
@@ -14,20 +14,22 @@ class Solution {
                     break;
                 }
             }
-            dp[0][j] = flag;
+            prev[j] = flag;
         }
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (str[i-1] == pat[j-1] || pat[j-1] == '?') {
-                    dp[i][j] = dp[i - 1][j - 1];
+            vector<bool> curr(m + 1, false);
+             for (int j = 1; j <= m; j++) {
+                if (str[i - 1] == pat[j - 1] || pat[j - 1] == '?') {
+                    curr[j] = prev[j - 1];
                 }
 
-                else if (pat[j-1] == '*') {
-                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                else if (pat[j - 1] == '*') {
+                    curr[j] = curr[j - 1] || prev[j];
                 }
             }
+            prev = curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
