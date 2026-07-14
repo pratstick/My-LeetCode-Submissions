df85a3ff20e5a77476c3852c46bf73c8cc6c3d28
Pratyush <pratyushk2005@outlook.com>
2026-07-14 22:19:01 +0530

    Time: 58 ms (40.25%) | Memory: 15.7 MB (66.77%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,37 +1,33 @@
 
 class Solution {
-private:
-    bool helper(int i, int j, const string& str, const string& pat, vector<vector<int>>& dp) {
-        
-        if (i < 0 && j < 0) return true;
-        if (i >= 0 && j < 0) return false;
-        if (i < 0 && j >= 0) {
-            for (int k = 0; k <= j; k++) {
-                if (pat[k] != '*') return false;
-            }
-            return true;
-        }
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-
-        if (str[i] == pat[j] || pat[j] == '?') {
-            return dp[i][j] = helper(i - 1, j - 1, str, pat, dp);
-        }
-
-        if (pat[j] == '*') {
-            return dp[i][j] = helper(i, j - 1, str, pat, dp) || helper(i - 1, j, str, pat, dp);
-        }
-
-        return dp[i][j] = false;
-    }
-    
-public:
+   public:
     bool isMatch(string str, string pat) {
-        int n = str.length();
-        int m = pat.length();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        
-        return helper(n - 1, m - 1, str, pat, dp);
+        int n = str.size();
+        int m = pat.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        dp[0][0] = true;
+        for (int j = 1; j <= m; j++) {
+            bool flag = true;
+            for (int k = 1; k <= j; k++) {
+                if (pat[k - 1] != '*') {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][j] = flag;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (str[i-1] == pat[j-1] || pat[j-1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+
+                else if (pat[j-1] == '*') {
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
