45333b0c2b6d2a9784e809c9f62f8e5527229d9f
Pratyush <pratyushk2005@outlook.com>
2026-07-14 11:36:39 +0530

    Time: 1337 ms (15.65%) | Memory: 386.1 MB (22.61%) - LeetSync

--- 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -4,37 +4,35 @@ public:
 
     int subsequencePairCount(vector<int>& nums) {
         int n = nums.size();
-
-        vector<vector<vector<int>>> dp(
-            n + 1, vector<vector<int>>(201, vector<int>(201, 0)));
-
-        dp[0][0][0] = 1;
-
+        vector<vector<int>>prev(201, vector<int>(201, 0));
+        prev[0][0] = 1;
         for (int ind = 0; ind < n; ind++) {
+            vector<vector<int>>curr(201, vector<int>(201, 0));
             for (int g1 = 0; g1 <= 200; g1++) {
                 for (int g2 = 0; g2 <= 200; g2++) {
 
-                    long long cur = dp[ind][g1][g2];
+                    long long cur = prev[g1][g2];
                     if (cur == 0)
                         continue;
 
                     // Not take
-                    dp[ind + 1][g1][g2] = (dp[ind + 1][g1][g2] + cur) % MOD;
+                    curr[g1][g2] = (curr[g1][g2] + cur) % MOD;
 
                     // Take in seq1
                     int ng1 = (g1 == 0) ? nums[ind] : gcd(g1, nums[ind]);
-                    dp[ind + 1][ng1][g2] = (dp[ind + 1][ng1][g2] + cur) % MOD;
+                    curr[ng1][g2] = (curr[ng1][g2] + cur) % MOD;
 
                     // Take in seq2
                     int ng2 = (g2 == 0) ? nums[ind] : gcd(g2, nums[ind]);
-                    dp[ind + 1][g1][ng2] = (dp[ind + 1][g1][ng2] + cur) % MOD;
+                    curr[g1][ng2] = (curr[g1][ng2] + cur) % MOD;
                 }
             }
+            prev = curr;
         }
 
         long long ans = 0;
         for (int g = 1; g <= 200; g++)
-            ans = (ans + dp[n][g][g]) % MOD;
+            ans = (ans + prev[g][g]) % MOD;
 
         return ans;
     }
