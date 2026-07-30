6263c927f28f4a5575fb4b7a8202b2a7bcd8358a
Pratyush <pratyushk2005@outlook.com>
2026-07-30 19:14:21 +0530

    Time: 38 ms (97.69%) | Memory: 61.6 MB (96.17%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -1,17 +1,15 @@
 class Solution {
 private:
-    // dis between cows is >= d
     bool canPlace(int d, const vector<int>& nums, int k, int n) {
         int count = 1;
         int last = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             if (nums[i] - nums[last] >= d) {
                 count++;
                 last = i;
             }
+            if (count >= k) return true;
         }
-        if (count >= k)
-            return true;
         return false;
     }
 
@@ -19,8 +17,8 @@ public:
     int maxDistance(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        int low = 0;
-        int high = nums[n - 1] - nums[0];
+        int low = 1;
+        int high = (nums[n - 1] - nums[0]) / (k - 1);
         int ans = -1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
