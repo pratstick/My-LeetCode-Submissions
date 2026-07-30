9a2324e7156f6704b61861487d5d0b0051e36043
Pratyush <pratyushk2005@outlook.com>
2026-07-30 19:09:24 +0530

    Time: 61 ms (10.38%) | Memory: 61.7 MB (70.46%) - LeetSync

---
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    //dis between cows is >= d
+    bool canPlace(int d, vector<int> &nums, int k,int n){
+        int count = 1;
+        int last = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] - nums[last] >= d){
+                count++;
+                last = i;
+            }
+        }
+        if(count >= k) return true;
+        return false;
+    }
+public:
+    int maxDistance(vector<int> &nums, int k) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        int low = 0;
+        int high = nums[n-1] - nums[0];
+        int ans = -1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(canPlace(mid,nums,k,n)){
+                ans = mid;
+                low = mid + 1;
+            }
+            else high = mid - 1;
+        }
+        return ans;
+    }
+};
