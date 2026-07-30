e302845662e20fa63656646a858116cfc6912753
Pratyush <pratyushk2005@outlook.com>
2026-07-30 18:30:08 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (8.26%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,7 +1,31 @@
 class Solution {
 public:
     bool search(vector<int> &nums, int k)  {
-      auto it = find(nums.begin(),nums.end(),k);
-      return it!=nums.end();
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        while(low<=high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid]==k) return true;
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+            if(nums[low] <= nums[mid]){
+                if(k<=nums[mid] && k>=nums[low]){
+                    high = mid - 1;
+                }
+                else low = mid + 1;
+            }
+            else{
+                if(k>nums[mid] && k<=nums[high]){
+                    low = mid + 1;
+                }
+                else high = mid - 1;
+            }
+        }
+
+        return false;
     }
 };
