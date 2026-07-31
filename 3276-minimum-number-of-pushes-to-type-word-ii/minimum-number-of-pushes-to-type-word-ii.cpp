cab694ca864038e48cd18009d36d725e5d2bc60e
Pratyush <pratyushk2005@outlook.com>
2026-07-31 06:36:51 +0530

    Time: 35 ms (21.97%) | Memory: 29.6 MB (5.99%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        unordered_map<char,int>mpp;
+        for(char c:word)mpp[c]++;
+        int ans = 0;
+        vector<int> freq;
+        for(auto&it:mpp){
+            freq.push_back(it.second);
+        }
+        sort(freq.rbegin(),freq.rend());
+        for(int i=0;i<freq.size();i++){
+            if(i<=7) ans+=freq[i];
+            else if(i<=15) ans+=(freq[i]*2);
+            else if(i<=23) ans+=(freq[i]*3);
+            else ans+=(freq[i]*4);
+        }
+        return ans;
+
+    }
+};
