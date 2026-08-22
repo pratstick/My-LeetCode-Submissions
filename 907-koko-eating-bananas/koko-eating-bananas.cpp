d2faf7421c99409d4f74d1719ce36a583fd8f918
Pratyush <pratyushk2005@outlook.com>
2026-08-22 12:30:20 +0530

    Time: 4 ms (86.94%) | Memory: 22.9 MB (57.03%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,29 +1,27 @@
+using ll = long long;
 class Solution {
 private:
-    long long timeFinish(int t, vector<int>& nums) {
-        long long time = 0;
-        for (int num : nums) {
-            time += (num + (long long)t - 1) / t;
+    ll timetoFinish(vector<int>&nums, int k){
+        ll time = 0;
+        for(int&it:nums){
+            time += ((it + k - 1)/k);
         }
         return time;
     }
-
 public:
-    int minEatingSpeed(vector<int>& nums, int h) {
+int minEatingSpeed(vector<int>& nums, int h) {
         int low = 1;
-        int high = *max_element(nums.begin(), nums.end());
-        int ans = high;
-
-        while (low <= high) {
+        int high = *max_element(nums.begin(),nums.end());
+        int ans = INT_MAX;
+        while(low <= high){
             int mid = low + (high - low) / 2;
-            long long time = timeFinish(mid, nums);
-
-            if (time <= h) {
+            ll time = timetoFinish(nums,mid);
+            
+            if(time <= h){
                 ans = mid;
                 high = mid - 1;
-            } else {
-                low = mid + 1; 
             }
+            else low = mid + 1;
         }
         return ans;
     }
