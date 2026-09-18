0e22a7dc7084fef2987e27ca2cb11a1f0292f456
Pratyush <pratyushk2005@outlook.com>
2026-09-18 18:21:19 +0530

    Time: 3 ms (36.19%) | Memory: 20.5 MB (29.97%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,57 +1,31 @@
-class DisjointSet {
-private:
-    vector<int> rank;
-    vector<int> parent;
-
-public:
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        for (int i = 0; i <= n; i++)
-            parent[i] = i;
-    }
-    int findParent(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findParent(parent[node]);
-    }
-    bool find(int u, int v) { return (findParent(u) == findParent(v)); }
-
-    void unionByRank(int u, int v) {
-        int pu = findParent(u);
-        int pv = findParent(v);
-        if (pu == pv)
-            return;
-        if (rank[pu] < rank[pv]) {
-            parent[pu] = pv;
-        } else if (rank[pu] == rank[pv]) {
-            parent[pv] = pu;
-            rank[pu]++;
-        } else {
-            parent[pv] = pu;
-        }
-    }
-};
-
 class Solution {
-public:
-    int findCircleNum(vector<vector<int>>& adj) {
-        unordered_set<int> set;
-        int n = adj.size();
-        DisjointSet ds(n);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (adj[i][j] == 1) {
-                    if (i < j)
-                        ds.unionByRank(i, j);
-                }
+   private:
+    void dfs(int node, vector<int>& vis, vector<vector<int>>& adjList) {
+        vis[node] = 1;
+        for (auto& ngh : adjList[node]) {
+            if (!vis[ngh]) {
+                dfs(ngh, vis, adjList);
             }
         }
-        for (int i = 0; i < n; i++) {
-            int p = ds.findParent(i);
-            set.insert(p);
-        }
+    }
 
-        return set.size();
+   public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int V = isConnected.size();
+        vector<vector<int>> adjList(V);
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < isConnected[i].size(); j++) {
+                if (isConnected[i][j] == 1) adjList[i].push_back(j);
+            }
+        }
+        vector<int> vis(V, 0);
+        int count = 0;
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                count++;
+                dfs(i, vis, adjList);
+            }
+        }
+        return count;
     }
 };
